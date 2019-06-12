0ee9a649f0e065e58ee2284aadd0b7e86da86e47
MelarDev <melardev@users.noreply.github.com>
2019-06-12 18:58:36 +0200

    Initial commit

---
+++ INetService.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace CoreBindShell
+{
+    interface INetService
+    {
+        void Start(IPAddress ipAddress, int port);
+        void WriteLine(int clientId, string output);
+        void AcceptOneClient();
+        void InteractAsync(int clientId);
+        void ReadSync(int clientId);
+        void Write(int clientId, string output);
+        void Shutdown();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreBindShell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
